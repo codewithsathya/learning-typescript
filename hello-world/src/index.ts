1ac894b3d197a3c4b7f96ab1a3d070f5313b7a7c
Sathiya Seelan <codewithsathya@gmail.com>
2022-09-01 18:52:34 +0530

    built-in type

---
+++ hello-world/src/index.ts
@@ -0,0 +1,8 @@
+let sales = 123_456_789;
+let course = "Typescript";
+let is_published: boolean = true;
+course = 9; //produces an error
+
+let level;
+level = "10";
+level = 11; // doesn't produce an error
