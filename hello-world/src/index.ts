ff6b05a5e668aaa432f255c60735d554814d4bad
Sathiya Seelan <codewithsathya@gmail.com>
2022-09-02 13:57:24 +0530

    Enums 4

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,4 +1,4 @@
 // enum Days { Sunday = true, Monday = true, Tuesday = true, Wednesday = true, Thursday = true, Friday = true, Saturday = true };//produces error
-enum Sizes { Small = 8.13, Medium, Large };
+enum Sizes { Small = 9, Medium = 9,  Large};
 console.log(Sizes.Small); //prints 8.13
 console.log(Sizes.Medium); //prints 9.13
