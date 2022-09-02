c5f079b087aac4fdac89092a13d24339eeda95b5
Sathiya Seelan <codewithsathya@gmail.com>
2022-09-02 11:33:47 +0530

    Enums 1

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,6 +1,4 @@
-let user: [string, number]; // tuples and can have more than 2 but not suggested.
-user = ["Sathya", 21]; // provides code completion for each element
-user = ["Sathya", "21"] // produces error
-user = ["Sathya", 21, 100] // produces error
-
-user.push("hello"); // doesn't produce error(drawback)
+enum Size { Small, Medium, Large };
+let mySize: Size = Size.Small;
+console.log(mySize); // prints 0;
+console.log(Size.Medium); //prints 1;
