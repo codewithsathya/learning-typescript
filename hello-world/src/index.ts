6fb3ff3f4031846f41a488b90fbebb3f6e1882af
Sathiya Seelan <codewithsathya@gmail.com>
2022-09-02 10:52:29 +0530

    Arrays 1

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,8 +1,2 @@
-let sales = 123_456_789;
-let course = "Typescript";
-let is_published: boolean = true;
-course = 9; //produces an error
-
-let level;
-level = "10";
-level = 11; // doesn't produce an error
+let arr = [10, "10"]; // now it can only store numbers and strings
+arr.push(true); // produces error
