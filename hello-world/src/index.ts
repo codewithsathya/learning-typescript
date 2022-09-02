05fa9b8361c29ea627fc330c085adc997e1cab49
Sathiya Seelan <codewithsathya@gmail.com>
2022-09-02 11:34:26 +0530

    Enums 2

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,4 +1,4 @@
-enum Size { Small, Medium, Large };
+enum Size { Small = 0, Medium, Large };
 let mySize: Size = Size.Small;
 console.log(mySize); // prints 0;
 console.log(Size.Medium); //prints 1;
