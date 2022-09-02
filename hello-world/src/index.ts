72dacfdedb0121c5e5c129fb0f10c19abd21bf9e
Sathiya Seelan <codewithsathya@gmail.com>
2022-09-02 15:38:08 +0530

    types

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,9 +1,11 @@
-let employee: {
+type Employee = {
   readonly id: number;
   name: string;
   age?: number;
   retire: (date: Date) => void;
-} = {
+}
+
+let employee: Employee = {
   id: 1,
   name: "Sathya",
   retire: (date) => date.getTime()
