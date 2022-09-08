eac704b653946d59c056c4470aa627b85f80a112
Sathiya Seelan <codewithsathya@gmail.com>
2022-09-08 17:45:11 +0530

    Optional chaining

--- hello-world/dist/index.js
+++ hello-world/dist/index.js
@@ -1,7 +1,10 @@
 "use strict";
-let employee = {
-    id: 1,
-    name: "Sathya",
-    retire: (date) => date.getTime()
-};
+var _a;
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
+}
+let customer = getCustomer(0);
+let time = (_a = customer === null || customer === void 0 ? void 0 : customer.birthday) === null || _a === void 0 ? void 0 : _a.getTime();
+let customers = [];
+console.log(customers[0]);
 //# sourceMappingURL=index.js.map
--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,8 +1,29 @@
-function greet(name: string | null | undefined){
-  if(name)
-    console.log(name.toUpperCase());
-  else
-    console.log("Stupid input");
+type Customer = {
+  birthday: Date | null
 }
 
-greet(null);
+function getCustomer(id: number): Customer | null{
+  return id === 0 ? null : { birthday: new Date() }
+}
+
+let customer = getCustomer(0);
+// Optional property access operator
+let time = customer?.birthday?.getTime();
+
+
+
+let customers: number[] | null;
+if(Date.now() > 10010010){
+  customers = null
+}else{
+  customers = [];
+}
+
+//Optional element access operator
+console.log(customers?.[0]);
+
+let log: any = null;
+log(); // doesn't show error & shows undefined when we run this
+
+//Optional call access operator
+log?.();
