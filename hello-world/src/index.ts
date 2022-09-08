2919ad2314cb7821fce8d4954c08198754f17f98
Sathiya Seelan <codewithsathya@gmail.com>
2022-09-08 18:04:58 +0530

    Nullish coalescing operator

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,29 +1,7 @@
-type Customer = {
-  birthday: Date | null
+let speed: number | null = null;
+
+let ride = {
+  // Falsy (undefined, null, '', false, 0)
+  // Nullish coalescing operator
+  speed: speed ?? 30
 }
-
-function getCustomer(id: number): Customer | null{
-  return id === 0 ? null : { birthday: new Date() }
-}
-
-let customer = getCustomer(0);
-// Optional property access operator
-let time = customer?.birthday?.getTime();
-
-
-
-let customers: number[] | null;
-if(Date.now() > 10010010){
-  customers = null
-}else{
-  customers = [];
-}
-
-//Optional element access operator
-console.log(customers?.[0]);
-
-let log: any = null;
-log(); // doesn't show error & shows undefined when we run this
-
-//Optional call access operator
-log?.();
