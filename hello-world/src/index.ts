2ae997389d2f950dea3f98e385320194eb07b7f3
Sathiya Seelan <codewithsathya@gmail.com>
2022-09-08 19:46:19 +0530

    Parameter properties

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,32 +1,41 @@
-class Account{
-  readonly id: number; // cannot change this
-  owner: string
-  private _balance: number //usually names start with _
+class Account {
   number?: number // optional property
+  // readonly id: number; // cannot change this
+  // owner: string
+  // private _balance: number //usually names start with _
 
-  constructor(id: number, owner: string, balance: number){
-    this.id = id;
-    this.owner = owner;
-    this._balance = balance;
+  // constructor(id: number, owner: string, balance: number){
+  //   this.id = id;
+  //   this.owner = owner;
+  //   this._balance = balance;
+  // }
+
+  // The above code can be written as below
+  constructor(
+    public readonly id: number, 
+    public owner: string, 
+    private _balance: number,
+    public phone?: number,
+    public email?: string) {
+
   }
 
-  deposit(amount: number): void{
-    if(amount <= 0){
+  deposit(amount: number): void {
+    if (amount <= 0) {
       throw new Error("Invalid amount");
     }
     this._balance += amount;
   }
 
-  getBalance(): number{
+  getBalance(): number {
     return this._balance;
   }
 
-  private _calculateTax(){ // we can also set private methods
+  private _calculateTax() { 
 
   }
 }
 
 let account = new Account(1, "Sathya", 0);
 account.deposit(100);
-// console.log(account._balance); // shows error
 console.log(account.getBalance());
