92f585c2f600c68d505e784bbd2b88edc98c57ed
Sathiya Seelan <codewithsathya@gmail.com>
2022-09-08 18:17:58 +0530

    Nullish coalescing operator

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -2,6 +2,7 @@ let speed: number | null = null;
 
 let ride = {
   // Falsy (undefined, null, '', false, 0)
+  //speed: speed || 30 // here if speed is 0, then speed property will be set to 30.
   // Nullish coalescing operator
   speed: speed ?? 30
 }
